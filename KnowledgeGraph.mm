4867c82caff4439ec0a1200fce663acd35a3b27c
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-09-26 11:37:12 -0400

    add new ontologies

---
+++ KnowledgeGraph.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Knowledge Graph" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1695691506695"><hook NAME="MapStyle" background="#f5f5dc">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sepia_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="The Art of Understanding" POSITION="bottom_or_right" ID="ID_1001622709" CREATED="1695691532687" MODIFIED="1695691539037">
+<node TEXT="Understanding" ID="ID_1820242084" CREATED="1695691561281" MODIFIED="1695691573020">
+<node TEXT="Understanding (in general) is the ability to grasp the meaning of information" ID="ID_1114068425" CREATED="1695691574274" MODIFIED="1695691594117"/>
+<node TEXT="Information is conveyed in the message using a specific language from a sender to a receiver." ID="ID_1607261938" CREATED="1695691594745" MODIFIED="1695691624630"/>
+<node TEXT="Information is understood by the receiver of a message, if the receiver interprets the information correctly." ID="ID_757061248" CREATED="1695691625007" MODIFIED="1695691648271"/>
+</node>
+<node TEXT="Correct Interpretation depends on" ID="ID_912583479" CREATED="1695691673968" MODIFIED="1695691684526">
+<node TEXT="Syntax" ID="ID_948063388" CREATED="1695691684912" MODIFIED="1695691688273">
+<node TEXT="&quot;=[greek]&quot; Arrangement, Ordering" ID="ID_1814419843" CREATED="1695691729428" MODIFIED="1695691743428"/>
+<node TEXT="In grammatics syntax denotes the study of the principles and processes by which sentences are constructed in particular languages" ID="ID_1829333467" CREATED="1695691746435" MODIFIED="1695691781433"/>
+<node TEXT="In formal languages, syntax is just a set of rules, by which well formed expressions can be created from a fundamental set of symbols (alphabet)" ID="ID_6907234" CREATED="1695691781595" MODIFIED="1695691820194"/>
+<node TEXT="In computer science, syntax defines the normative structure of data" ID="ID_1848784978" CREATED="1695691820365" MODIFIED="1695691835256"/>
+</node>
+<node TEXT="Semantics" ID="ID_1877777514" CREATED="1695691688451" MODIFIED="1695691691973">
+<node TEXT="&quot;=[greek]&quot; pertains to the character, the study of meaning" ID="ID_47611404" CREATED="1695691850898" MODIFIED="1695691867513"/>
+<node TEXT="is part of the linguistics which focuses on Sense and Meaning of language or symbols of language" ID="ID_1100389420" CREATED="1695691867940" MODIFIED="1695691889413"/>
+<node TEXT="is the study of interpretation of signs or symbols as used by agents or communities within particular circumstances and contexts" ID="ID_1861920615" CREATED="1695691889571" MODIFIED="1695691915111"/>
+<node TEXT="Semantics asks, how sense and meaning of complex concepts can be derived from simple concepts based on the rules of syntax" ID="ID_1927665570" CREATED="1695691915267" MODIFIED="1695691950898"/>
+<node TEXT="The semantics of a message depends on context and pragmatics" ID="ID_1587209367" CREATED="1695691951052" MODIFIED="1695691973929"/>
+</node>
+<node TEXT="Context" ID="ID_427501075" CREATED="1695691692131" MODIFIED="1695691694834">
+<node TEXT="&quot;[lat.]&quot; contexgus = interweaved" ID="ID_268171303" CREATED="1695691983451" MODIFIED="1695692010949"/>
+<node TEXT="denotes the surrounding of a symbol (concept) in an expression resp. its relationship with surrounding expressions (concepts) and further related elements" ID="ID_1735019133" CREATED="1695692011115" MODIFIED="1695692052870"/>
+<node TEXT="Contexts denotes all elements of any sort of communication that define the interpretation of the communicated content." ID="ID_1071561819" CREATED="1695692053284" MODIFIED="1695692085694"/>
+</node>
+<node TEXT="Pragmatics" ID="ID_87506481" CREATED="1695691694974" MODIFIED="1695691697865">
+<node TEXT="&quot;=[greek]&quot; action" ID="ID_97384657" CREATED="1695692102300" MODIFIED="1695692131028"/>
+<node TEXT="reflects the intention by which the language is used to communicate a message" ID="ID_48695143" CREATED="1695692131530" MODIFIED="1695692147403"/>
+<node TEXT="In linguistics, pragmatics denotes the study of applying language in different situations" ID="ID_105512164" CREATED="1695692147748" MODIFIED="1695692168520"/>
+<node TEXT="It also denotes the intended purpose of the speaker" ID="ID_1477834143" CREATED="1695692168696" MODIFIED="1695692178070"/>
+<node TEXT="Pragmatics studies the way in which context contributes to meaning" ID="ID_1916193230" CREATED="1695692249649" MODIFIED="1695692269018"/>
+</node>
+<node TEXT="Experience" ID="ID_1263291405" CREATED="1695691698051" MODIFIED="1695691700255">
+<node TEXT="Experience considers all information that you have learned and put in context with the world you are living in" ID="ID_1189806111" CREATED="1695692275608" MODIFIED="1695692317870"/>
+<node TEXT="Experience in this sense is often referred to as common sense knowledge or world knowledge" ID="ID_955708354" CREATED="1695692318385" MODIFIED="1695692336864"/>
+</node>
+</node>
+<node TEXT="Successful Communication" ID="ID_1817399192" CREATED="1695692352580" MODIFIED="1695692360053">
+<node TEXT="Information has to be correctly transmitted (Syntax)" ID="ID_1168869707" CREATED="1695692360057" MODIFIED="1695692374400"/>
+<node TEXT="The meaning (Semantics) of the transmitted information must be interpreted correctly (= understanding)" ID="ID_1409642673" CREATED="1695692376812" MODIFIED="1695692411111"/>
+<node TEXT="Understanding depends on" ID="ID_238521475" CREATED="1695692415461" MODIFIED="1695692425087">
+<node TEXT="the context of both sender and receiver and" ID="ID_1164327258" CREATED="1695692425431" MODIFIED="1695692434603"/>
+<node TEXT="the pragmatics of the sender" ID="ID_920521127" CREATED="1695692435351" MODIFIED="1695692441023"/>
+</node>
+<node TEXT="(Personal) experience determines how sender and receiver interpret the semantics, context, and pragmatics of a message, and thus its intended meaning" ID="ID_1720764518" CREATED="1695692441885" MODIFIED="1695692481633"/>
+</node>
+</node>
+</node>
+</map>
