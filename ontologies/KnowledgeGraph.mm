627a8b376064fc79c0a4a0f8e32e0126ace3c5b3
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-11-18 11:01:06 -0500

    revise structure

---
+++ ontologies/KnowledgeGraph.mm
@@ -0,0 +1,171 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Knowledge Graph" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1695755967065" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#f5f5dc">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-9.75 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Knowledge Graph in the Web of Data" POSITION="bottom_or_right" ID="ID_165789378" CREATED="1695755974238" MODIFIED="1695756064788">
+<node TEXT="The Art of Understanding" FOLDED="true" ID="ID_1001622709" CREATED="1695691532687" MODIFIED="1695691539037">
+<node TEXT="Understanding" ID="ID_1820242084" CREATED="1695691561281" MODIFIED="1695691573020">
+<node TEXT="Understanding (in general) is the ability to grasp the meaning of information" ID="ID_1114068425" CREATED="1695691574274" MODIFIED="1695691594117"/>
+<node TEXT="Information is conveyed in the message using a specific language from a sender to a receiver." ID="ID_1607261938" CREATED="1695691594745" MODIFIED="1695691624630"/>
+<node TEXT="Information is understood by the receiver of a message, if the receiver interprets the information correctly." ID="ID_757061248" CREATED="1695691625007" MODIFIED="1695691648271"/>
+</node>
+<node TEXT="Correct Interpretation depends on" ID="ID_912583479" CREATED="1695691673968" MODIFIED="1695691684526">
+<node TEXT="Syntax" ID="ID_948063388" CREATED="1695691684912" MODIFIED="1695691688273">
+<node TEXT="&quot;=[greek]&quot; Arrangement, Ordering" ID="ID_1814419843" CREATED="1695691729428" MODIFIED="1695691743428"/>
+<node TEXT="In grammatics syntax denotes the study of the principles and processes by which sentences are constructed in particular languages" ID="ID_1829333467" CREATED="1695691746435" MODIFIED="1695691781433"/>
+<node TEXT="In formal languages, syntax is just a set of rules, by which well formed expressions can be created from a fundamental set of symbols (alphabet)" ID="ID_6907234" CREATED="1695691781595" MODIFIED="1695691820194"/>
+<node TEXT="In computer science, syntax defines the normative structure of data" ID="ID_1848784978" CREATED="1695691820365" MODIFIED="1695691835256"/>
+</node>
+<node TEXT="Semantics" ID="ID_1877777514" CREATED="1695691688451" MODIFIED="1695691691973">
+<node TEXT="&quot;=[greek]&quot; pertains to the character, the study of meaning" ID="ID_47611404" CREATED="1695691850898" MODIFIED="1695691867513"/>
+<node TEXT="is part of the linguistics which focuses on Sense and Meaning of language or symbols of language" ID="ID_1100389420" CREATED="1695691867940" MODIFIED="1695691889413"/>
+<node TEXT="is the study of interpretation of signs or symbols as used by agents or communities within particular circumstances and contexts" ID="ID_1861920615" CREATED="1695691889571" MODIFIED="1695691915111"/>
+<node TEXT="Semantics asks, how sense and meaning of complex concepts can be derived from simple concepts based on the rules of syntax" ID="ID_1927665570" CREATED="1695691915267" MODIFIED="1695691950898"/>
+<node TEXT="The semantics of a message depends on context and pragmatics" ID="ID_1587209367" CREATED="1695691951052" MODIFIED="1695691973929"/>
+</node>
+<node TEXT="Context" ID="ID_427501075" CREATED="1695691692131" MODIFIED="1695691694834">
+<node TEXT="&quot;[lat.]&quot; contexgus = interweaved" ID="ID_268171303" CREATED="1695691983451" MODIFIED="1695692010949"/>
+<node TEXT="denotes the surrounding of a symbol (concept) in an expression resp. its relationship with surrounding expressions (concepts) and further related elements" ID="ID_1735019133" CREATED="1695692011115" MODIFIED="1695692052870"/>
+<node TEXT="Contexts denotes all elements of any sort of communication that define the interpretation of the communicated content." ID="ID_1071561819" CREATED="1695692053284" MODIFIED="1695692085694"/>
+</node>
+<node TEXT="Pragmatics" ID="ID_87506481" CREATED="1695691694974" MODIFIED="1695691697865">
+<node TEXT="&quot;=[greek]&quot; action" ID="ID_97384657" CREATED="1695692102300" MODIFIED="1695692131028"/>
+<node TEXT="reflects the intention by which the language is used to communicate a message" ID="ID_48695143" CREATED="1695692131530" MODIFIED="1695692147403"/>
+<node TEXT="In linguistics, pragmatics denotes the study of applying language in different situations" ID="ID_105512164" CREATED="1695692147748" MODIFIED="1695692168520"/>
+<node TEXT="It also denotes the intended purpose of the speaker" ID="ID_1477834143" CREATED="1695692168696" MODIFIED="1695692178070"/>
+<node TEXT="Pragmatics studies the way in which context contributes to meaning" ID="ID_1916193230" CREATED="1695692249649" MODIFIED="1695692269018"/>
+</node>
+<node TEXT="Experience" ID="ID_1263291405" CREATED="1695691698051" MODIFIED="1695691700255">
+<node TEXT="Experience considers all information that you have learned and put in context with the world you are living in" ID="ID_1189806111" CREATED="1695692275608" MODIFIED="1695692317870"/>
+<node TEXT="Experience in this sense is often referred to as common sense knowledge or world knowledge" ID="ID_955708354" CREATED="1695692318385" MODIFIED="1695692336864"/>
+</node>
+</node>
+<node TEXT="Successful Communication" ID="ID_1817399192" CREATED="1695692352580" MODIFIED="1695692360053">
+<node TEXT="Information has to be correctly transmitted (Syntax)" ID="ID_1168869707" CREATED="1695692360057" MODIFIED="1695692374400"/>
+<node TEXT="The meaning (Semantics) of the transmitted information must be interpreted correctly (= understanding)" ID="ID_1409642673" CREATED="1695692376812" MODIFIED="1695692411111"/>
+<node TEXT="Understanding depends on" ID="ID_238521475" CREATED="1695692415461" MODIFIED="1695692425087">
+<node TEXT="the context of both sender and receiver and" ID="ID_1164327258" CREATED="1695692425431" MODIFIED="1695692434603"/>
+<node TEXT="the pragmatics of the sender" ID="ID_920521127" CREATED="1695692435351" MODIFIED="1695692441023"/>
+</node>
+<node TEXT="(Personal) experience determines how sender and receiver interpret the semantics, context, and pragmatics of a message, and thus its intended meaning" ID="ID_1720764518" CREATED="1695692441885" MODIFIED="1695692481633"/>
+</node>
+</node>
+<node TEXT="Towards a Universal Knowledge Representation" ID="ID_225357650" CREATED="1695755938201" MODIFIED="1695755967061">
+<node TEXT="Entity processing" ID="ID_787142215" CREATED="1695762593859" MODIFIED="1695762598372">
+<node TEXT="Entity Typing is the task of labeling a given entity mention in tex with a semantic label" POSITION="bottom_or_right" ID="ID_289126324" CREATED="1695762569287" MODIFIED="1695762593292"/>
+<node TEXT="Entity Resolution locates entity terms in text and links them to manifestations of real world objects" POSITION="bottom_or_right" ID="ID_1488696262" CREATED="1695762523457" MODIFIED="1695762556211"/>
+<node TEXT="Word Sense Disambiguation helps to identify which sense of a word is used in a sentence" POSITION="bottom_or_right" ID="ID_564554588" CREATED="1695762619363" MODIFIED="1695762643209"/>
+<node TEXT="Relation Extraction is the detection and classification of semantic relationship mentions within a set of artifacts, typically from text" POSITION="bottom_or_right" ID="ID_31585663" CREATED="1695762649116" MODIFIED="1695762677781"/>
+<node TEXT="Syntax defines the normative structure of message" POSITION="bottom_or_right" ID="ID_1605274700" CREATED="1695762829580" MODIFIED="1695762842396"/>
+</node>
+<node TEXT="The Meaning of Information" ID="ID_448779378" CREATED="1695762697742" MODIFIED="1695762709476">
+<node TEXT="The meaning defines what the elements of a message represent" ID="ID_1030815464" CREATED="1695762709480" MODIFIED="1695762728613"/>
+<node TEXT="Machines are able to understand the meaning of information by using formal knowledge representation" ID="ID_1199348372" CREATED="1695762737035" MODIFIED="1695762755621"/>
+<node TEXT="To understand, the meaning (Semantic) of entities and classes must be defined explicitly" ID="ID_655164738" CREATED="1695762756748" MODIFIED="1695762791279"/>
+<node TEXT="Meaning can be expressed with the help of knowledge representations (Ontologies)" POSITION="bottom_or_right" ID="ID_936509996" CREATED="1695756086830" MODIFIED="1695762819942"/>
+</node>
+<node TEXT="Knowledge Representation vs Data Structure" ID="ID_1737396896" CREATED="1695756831721" MODIFIED="1695756844160">
+<node TEXT="Mathematical Logic provides a framework to formally express the semantics of knowledge representations" ID="ID_1488585016" CREATED="1695756844160" MODIFIED="1695756868260"/>
+<node TEXT="Semantics of knowledge representations can be defined explicitly." ID="ID_822165023" CREATED="1695756868433" MODIFIED="1695756882643"/>
+<node TEXT="Mathematical Logic enables logical inferences and reasoning for knowledge representations" ID="ID_441501604" CREATED="1695756883355" MODIFIED="1695756906879"/>
+</node>
+<node TEXT="The Semantic Web (a web of data)" ID="ID_494693449" CREATED="1695756918297" MODIFIED="1695762343368"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The Semantic Web is an extension of the current web in which information is given well-defined meaning, better enabling computers and people to work in cooperation.
+    </p>
+    <p>
+      By Tim Berners-Less, James Hendler, Ora Lassila at Scientific American, 2001
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="The Semantic Web is an Extension of the current web" ID="ID_864742775" CREATED="1695756929941" MODIFIED="1695756940876"/>
+<node TEXT="The meaning of information (Semantics) is made explicit by formal (structured) and standardized knowledge representations (Ontologies)" ID="ID_109736636" CREATED="1695756941384" MODIFIED="1695756975153"/>
+<node TEXT="Thereby it&apos;s possible to process the meaning of information automatically" ID="ID_1880125203" CREATED="1695756976832" MODIFIED="1695756995460"/>
+<node TEXT="It&apos;s possible to relate and integrate heterogeneous data" ID="ID_477563697" CREATED="1695756995663" MODIFIED="1695757008845"/>
+<node TEXT="It&apos;s possible to deduce implicit (not evident) information from existing (evident) information in an automated way" ID="ID_1906618614" CREATED="1695757009019" MODIFIED="1695757032949"/>
+<node TEXT="The Semantic Web is kind of a global database that contains a universal network of semantic propositions" ID="ID_26858583" CREATED="1695757033435" MODIFIED="1695757057709"/>
+</node>
+</node>
+</node>
+</node>
+</map>
