1b4db333b8fab269a2758e17e72994f4baf2c477
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-01 10:48:19 +0800

    add Data Quality

---
+++ DataQuality.mm
@@ -0,0 +1,270 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Data Quality&#xa;(6 dimensions)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1764555407843">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeeeff" zoom="0.6830134">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_grey_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_364653791" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#676767" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_364653791" STARTINCLINATION="122.25 pt;0 pt;" ENDINCLINATION="119.25 pt;35.25 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1533685614" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_1533685614"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#cc0033"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#222222" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Validity" POSITION="bottom_or_right" ID="ID_1879132021" CREATED="1764555446781" MODIFIED="1764555451518">
+<node ID="ID_1964086136" CREATED="1764555451525" MODIFIED="1764555951342"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Refers to how much the data <span style="font-weight: bold;">aligns with defined business rules or limitations</span>.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="It checks if the data is in the format it&apos;s expected to be." ID="ID_1246737478" CREATED="1764555474846" MODIFIED="1764555499599"/>
+<node ID="ID_982683258" CREATED="1764555503741" MODIFIED="1764555960566"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: When a machine error appears on the screen, having the error code <span style="font-weight: bold;">follow a standardized format</span>&#xa0;helps in quickly identifying the error type and starting the correct troubleshooting process
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Accuracy" POSITION="bottom_or_right" ID="ID_1520059101" CREATED="1764555572268" MODIFIED="1764555575613">
+<node ID="ID_752227993" CREATED="1764555575621" MODIFIED="1764555932195"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Refers to the degree of correctness and precision in <span style="font-weight: bold;">representing the true values of an object or event</span>.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="It describes how well the data reflects reality without errors or deviations" ID="ID_1027836961" CREATED="1764555599451" MODIFIED="1764555622479"/>
+<node ID="ID_1934656437" CREATED="1764555624748" MODIFIED="1764555941568"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: If an error <span style="font-weight: bold;">accurately displays a sensor temperature</span>&#xa0;reading of 75.3°C, exceeding the normal operational range, it signals that the machine is overheating and prompts the operator to take corrective action.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Consistency" POSITION="bottom_or_right" ID="ID_1381598284" CREATED="1764555784124" MODIFIED="1764555789293">
+<node ID="ID_90473430" CREATED="1764555789296" MODIFIED="1764555919924"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A measure to ensure that a record of data values remains <span style="font-weight: bold;">aligned or uniform across different sources, systems, and locations</span>&#xa0;when compared against each other or a reference dataset.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_27956729" CREATED="1764555839019" MODIFIED="1764555905682"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: consistency using the <span style="font-weight: bold;">same error messages across machines for similar faults</span>&#xa0;ensures that when you see a particular error, you can immediately apply knowledge from previous fixing the same issues.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Uniqueness" POSITION="top_or_left" ID="ID_693327283" CREATED="1764555962464" MODIFIED="1764555965662">
+<node TEXT="Refers to the absence of repeated information in the data." ID="ID_578042463" CREATED="1764555970605" MODIFIED="1764555988816"/>
+<node ID="ID_1521193130" CREATED="1764555989374" MODIFIED="1764556012992"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This means that <span style="font-weight: bold;">data is recorded only once in a system</span><span style="font-weight: normal;">, without duplication.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_691581812" CREATED="1764556013197" MODIFIED="1764556063729"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: A software solution at the plant uses <span style="font-weight: bold;">one unique error message for each distinct type of error</span><span style="font-weight: normal;">. This helps in identifying the current problem, avoiding confusion, and resulting in quicker diagnosis and resolution.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Timeliness" POSITION="top_or_left" ID="ID_1651111820" CREATED="1764556068556" MODIFIED="1764556073229">
+<node ID="ID_454933443" CREATED="1764556074269" MODIFIED="1764556107217"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Refers to whether the data is <span style="font-weight: bold;">up-to-date and available when needed </span><span style="font-weight: normal;">for a particular purpose or situation.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_281777210" CREATED="1764556108654" MODIFIED="1764556144782"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: The immediate display of an error message <span style="font-weight: bold;">as soon as a fault is detected</span><span style="font-weight: normal;">&#xa0;helps us decide quickly on corrective action, preventing further damage or downtime.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Completeness" POSITION="top_or_left" ID="ID_1627980340" CREATED="1764556149819" MODIFIED="1764556155119">
+<node TEXT="The extent to which all required and relevant data is present." ID="ID_1037083680" CREATED="1764556155337" MODIFIED="1764556170588"/>
+<node ID="ID_301510931" CREATED="1764556171051" MODIFIED="1764556203626"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A complete dataset gives the user a <span style="font-weight: bold;">full picture of the subject of interest</span><span style="font-weight: normal;">&#xa0;with all the necessary information, avoiding any misunderstanding.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1578238150" CREATED="1764556204268" MODIFIED="1764556272908"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Example: An error message that <span style="font-weight: bold;">includes complete data </span><span style="font-weight: normal;">- such as the affected component, the type of error, and the location of the machine - provides a comprehensive overview, enabling a more informed and faster response to the issue.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
